a24b37f012a0df31f51c70f9a54269cd3621b109
Parker Shumate <pshumate15@gmail.com>
2019-10-15 20:15:05 -0500

    Add ability to open streams

--- script.js
+++ script.js
@@ -39,28 +39,50 @@ function createLinkDiv(responseObj) {
 
     const div = document.createElement('div');
     const h2 = document.createElement('h2');
-    h2.textContent = responseObj.name;
+    h2.textContent = responseObj.hasOwnProperty('name') ? responseObj.name : responseObj.author;
 
-    if (responseObj.hasOwnProperty('links')) {
-        // Add event listener that gets next page when clicked
+    // If there's a streams property, then launch a stream on click
+    if (responseObj.hasOwnProperty('streams')) {
+        a.addEventListener('click', () => {
+            main.textContent = '';
+            const h1 = document.createElement('h1');
+            h1.textContent = responseObj.author;
+            main.appendChild(h1);
+        
+            const openingGameMessage = document.createElement('h3');
+            openingGameMessage.textContent = 'Opening game...';
+            main.appendChild(openingGameMessage);
+            
+            window.open(responseObj.streams[0], '_blank');
+        });
+    // If there's a links property, then there's more pages after the current page
+    } else if (responseObj.hasOwnProperty('links')) {
         a.addEventListener('click', () => {
             main.textContent = '';
             const h1 = document.createElement('h1');
             h1.textContent = responseObj.name;
             main.appendChild(h1);
-
+        
+            const loadingMessage = document.createElement('h3');
+            loadingMessage.textContent = 'Loading...';
+            main.appendChild(loadingMessage);
+            
             const xhr = new XMLHttpRequest();
             xhr.open('GET', responseObj.links[0].href);
-            xhr.onload = () => {
-                if (xhr.status !== 200) {
-                    main.textContent = 'Error occurred while getting content.';
-                } else {
-                    const pageObj = JSON.parse(xhr.response);
-
-                    if (pageObj.length === 0) {
-                        createLinkDiv({name: 'No games to display.'});
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status !== 200) {
+                        main.textContent = 'Error occurred while getting content.';
                     } else {
-                        pageObj.forEach(obj => createLinkDiv(obj));                    
+                        main.removeChild(loadingMessage);
+                        
+                        const pageObj = JSON.parse(xhr.response);
+    
+                        if (pageObj.length === 0) {
+                            createLinkDiv({name: 'No games to display.'});
+                        } else {
+                            pageObj.forEach(obj => createLinkDiv(obj));                    
+                        }
                     }
                 }
             };
